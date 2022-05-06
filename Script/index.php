b8296ef1b31ef40999c5c861b12ce2aefa789d86
METILLE Pierrot <Pierrot.METILLE@cpnv.ch>
2022-05-06 10:34:07 +0200

    Rewrite index

--- Script/index.php
+++ Script/index.php
@@ -1,14 +1,15 @@
+
 <?php
 /**
  * @file    users.php
  * @brief   Sent the user where he should be when clicking a button/link
- * @author  Create by Nathan.CHAUVEAU
- * @version 02.02.2022
+ * @author  Create by Nathan.CHAUVEAU modified by Adam, Elodie, Pierrot
+ * @version 06.05.2022
  */
 
-require 'Controller/users.php';
-require "Controller/navigation.php";
-require "Controller/product.php";
+require 'controller/users.php';
+require "controller/navigation.php";
+require "controller/product.php";
 //start a new session
 session_start();
 //depending where the user was , it will guide him in a new page
@@ -16,42 +17,50 @@ if (isset($_GET['action'])){
     $action=$_GET['action'];
     switch ($action) {
         case 'addarticle':
-            Addarticle($_POST);
+            viewAddArticle();
             break;
-        case 'controlarticle':
-            ControlArticle($_POST);
+        case 'addArticle':
+            $title= $_POST['titre'];
+            $category= $_POST['categorie'];
+            $description = $_POST['description'];
+            $image=$_POST['image'];
+            $price=$_POST['prix'];
+            addArticle($title,$category,$description,$image,$price);
             break;
         case 'check':
             check($_POST);
             break;
-        case 'create':
-            Newaccount();
+        case 'viewRegister':
+            viewRegister();
             break;
         //stop the session and sent back the user to home
         case 'unlogin':
-            SessionUnlogin();
+            sessionUnlogin();
             break;
         //collect the data from the formulaire, write the datas in the json file and sent back the user in the home page with a new message
-        case 'session':
-            SessionLogin($_POST);
+        case 'register':
+            $email = $_POST['email'];
+            $pseudo = $_POST['username'];
+            $pwd = $_POST['mdp'];
+            register($email,$pseudo,$pwd);
             break;
         //sent the user to the login page
-        case 'login':
-            Login();
+        case 'viewLogin':
+            viewLogin();
             break;
         //sent the user to the home page
         case 'home':
-            Home();
+            home();
             //send to home
             break;
         //sent the user to the lost page
         default:
-            Lost();
+            lost();
     }
 }
 else{
     //sent the user to the home page
-   Home();
+    home();
 
 }
 
